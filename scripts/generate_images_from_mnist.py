59849abdb1a8f492e41d1c69fb2eea2efedf4231
Raul Chandias Ferrari <chandiar@iro.umontreal.ca>
2013-04-26 09:46:57 -0400

    ...

--- scripts/generate_images_from_mnist.py
+++ scripts/generate_images_from_mnist.py
@@ -1,17 +1,31 @@
 # -*- coding: utf-8 -*-
 
+import os
 import sys
+import numpy
+import scipy
 import sklearn
 from sklearn.datasets import fetch_mldata
 
-import util
+from ml_code import util
 
 def main():
     # Load the MNIST dataset.
     data_dir = util.get_dataset_base_path()
     mnist = fetch_mldata('MNIST original',
         data_home=data_dir)
-
-
+    data = mnist.data
+    targets = mnist.target
+    import pdb; pdb.set_trace()
+    for idx, sample in enumerate(data):
+        sys.stdout.write('\r%2d%%' % int(idx / float(len(data)) * 100 + 0.5))
+        sys.stdout.flush()
+        sample = sample.reshape((28,28))
+        target = int(targets[idx])
+        path = os.path.join(data_dir, 'mnist_images', )
+        filename = 'image_idx_%s_number_%s.png'%(idx, target)
+        path = os.path.join(path, filename)
+        scipy.misc.imsave(path, sample)
+    
 if __name__=='__main__':
     sys.exit(main())
