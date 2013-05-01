3d233b2cd783576ddf685df50c3609901c82be11
Raul Chandias Ferrari <chandiar@iro.umontreal.ca>
2013-05-01 13:40:42 -0400

    Load data in shared variables.

--- ml_code/util.py
+++ ml_code/util.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import cPickle
+import gzip
+import numpy
 import os
 import StringIO
 import tarfile
+import theano
+from theano import tensor as T
 
 
 def dump_tar_bz2(obj, path):
@@ -36,7 +40,12 @@ def dump_tar_bz2(obj, path):
 
 
 def get_dataset_base_path():
-    return os.environ.get('ML_DATA_PATH')
+    data_dir = os.environ.get('ML_DATA_PATH')
+    if data_dir is None:
+        print 'Environment variable ML_DATA_PATH not set correctly!'
+        return 1
+    else:
+        return data_dir
 
 
 def load_pkl(path):
@@ -67,3 +76,81 @@ def load_tar_bz2(path):
     finally:
         f.close()
     return cPickle.loads(data)
+
+
+
+def load_data(dataset, splits=[50000, 10000, 10000], shared=False):
+    ''' Loads the dataset
+
+    :type dataset: string
+    :param dataset: the path to the dataset (here MNIST)
+    '''
+
+    #############
+    # LOAD DATA #
+    #############
+
+    # Download the MNIST dataset if it is not present
+    data_dir, data_file = os.path.split(dataset)
+    if (not os.path.isfile(dataset)) and data_file == 'mnist.pkl.gz':
+        import urllib
+        origin = 'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
+        print 'Downloading data from %s' % origin
+        urllib.urlretrieve(origin, dataset)
+
+    print '... loading data'
+
+    # Load the dataset
+    f = gzip.open(dataset, 'rb')
+    train_set, valid_set, test_set = cPickle.load(f)
+    f.close()
+
+    train_set = (train_set[0][:splits[0]], train_set[1][:splits[0]])
+    valid_set = (valid_set[0][0:splits[1]], valid_set[1][0:splits[1]])
+    test_set = (test_set[0][0:splits[2]], test_set[1][0:splits[2]])
+
+    #train_set, valid_set, test_set format: tuple(input, target)
+    #input is an numpy.ndarray of 2 dimensions (a matrix)
+    #witch row's correspond to an example. target is a
+    #numpy.ndarray of 1 dimensions (vector)) that have the same length as
+    #the number of rows in the input. It should give the target
+    #target to the example with the same index in the input.
+
+    def shared_dataset(data_xy, borrow=True):
+        """ Function that loads the dataset into shared variables
+
+        The reason we store our dataset in shared variables is to allow
+        Theano to copy it into the GPU memory (when code is run on GPU).
+        Since copying data into the GPU is slow, copying a minibatch everytime
+        is needed (the default behaviour if the data is not in a shared
+        variable) would lead to a large decrease in performance.
+        """
+        data_x, data_y = data_xy
+        shared_x = theano.shared(numpy.asarray(data_x,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        shared_y = theano.shared(numpy.asarray(data_y,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+        # When storing data on the GPU it has to be stored as floats
+        # therefore we will store the labels as ``floatX`` as well
+        # (``shared_y`` does exactly that). But during our computations
+        # we need them as ints (we use labels as index, and if they are
+        # floats it doesn't make sense) therefore instead of returning
+        # ``shared_y`` we will have to cast it to int. This little hack
+        # lets ous get around this issue
+        return shared_x, T.cast(shared_y, 'int32')
+
+    if shared:
+        test_set_x, test_set_y = shared_dataset(test_set)
+        valid_set_x, valid_set_y = shared_dataset(valid_set)
+        train_set_x, train_set_y = shared_dataset(train_set)
+    else:
+        test_set_x, test_set_y = test_set
+        valid_set_x, valid_set_y = valid_set
+        train_set_x, train_set_y = train_set
+
+    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
+            (test_set_x, test_set_y)]
+    return rval
+
