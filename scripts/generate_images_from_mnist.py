9843e7ee2bec3df568a6014924991cddffe3a73c
Raul Chandias Ferrari <chandiar@iro.umontreal.ca>
2013-05-01 13:41:14 -0400

    Use correct mnist dataset (the one from deeplearning).

--- scripts/generate_images_from_mnist.py
+++ scripts/generate_images_from_mnist.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import cPickle
+import gzip
 import os
 import sys
 import numpy
@@ -7,25 +9,47 @@ import scipy
 import sklearn
 from sklearn.datasets import fetch_mldata
 
-from ml_code import util
+import project2
+from project2.ml_code import util
+
+data_dir = util.get_dataset_base_path()
+
+def generate_images(data):
+    dataset = data[0][0]
+    label = data[0][1]
+    dataset_name = data[1]
+    print 'Generating images from %s' %dataset_name
+    print '%s shape is %s' %(dataset_name, dataset.shape)
+    print 'Label shape is %s' %label.shape
+    for idx, sample in enumerate(dataset):
+        sys.stdout.write('\r%2d%%' % int(idx / float(len(dataset)) * 100 + 0.5))
+        sys.stdout.flush()
+        sample = sample.reshape((28,28))
+        target = int(label[idx])
+        out_dir = os.path.join(data_dir, 'mnist_images', '%s'%dataset_name)
+        if not os.path.exists(out_dir):
+            print 'creating directory %s' %out_dir
+            os.makedirs(out_dir)
+        filename = 'image_idx_%s_number_%s.png'%(idx, target)
+        out_path = os.path.join(out_dir, filename)
+        if not os.path.exists(out_path):
+            scipy.misc.imsave(out_path, sample)
 
 def main():
     # Load the MNIST dataset.
-    data_dir = util.get_dataset_base_path()
-    mnist = fetch_mldata('MNIST original',
-        data_home=data_dir)
-    data = mnist.data
-    targets = mnist.target
+    #mnist = fetch_mldata('MNIST original',
+    #    data_home=data_dir)
+    data_path = os.path.join(data_dir, 'mnist.pkl.gz')
+    print 'Loading the MNIST dataset from %s' %data_path
+    f = gzip.open(data_path, 'rb')
+    train_set, valid_set, test_set = cPickle.load(f)
+    f.close()
     import pdb; pdb.set_trace()
-    for idx, sample in enumerate(data):
-        sys.stdout.write('\r%2d%%' % int(idx / float(len(data)) * 100 + 0.5))
-        sys.stdout.flush()
-        sample = sample.reshape((28,28))
-        target = int(targets[idx])
-        path = os.path.join(data_dir, 'mnist_images', )
-        filename = 'image_idx_%s_number_%s.png'%(idx, target)
-        path = os.path.join(path, filename)
-        scipy.misc.imsave(path, sample)
-    
+    generate_images((valid_set, 'valid_set'))
+    import pdb; pdb.set_trace()
+    for data in [(train_set, 'train_set'), (valid_set, 'valid_set'), (test_set, 'test_set')]:
+        generate_images(data)
+        import pdb; pdb.set_trace()
+
 if __name__=='__main__':
     sys.exit(main())
